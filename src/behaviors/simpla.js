ba6a5d330c230dcf65d23841b58fa58d9bd102a7
Sean King <sean@seanking.org>
2017-02-03 18:42:35 +1100

    add missing code comments

--- src/behaviors/simpla.js
+++ src/behaviors/simpla.js
@@ -1,10 +1,18 @@
 export default {
   properties: {
+    /**
+     * Simpla data ID
+     * @type {String}
+     */
     uid: {
       type: String,
       observer: '_initUid'
     },
 
+    /**
+     * Stored Simpla observers
+     * @type {Object}
+     */
     _observers: {
       type: Object,
       value: {}
@@ -15,10 +23,20 @@ export default {
     '_setData(_value, uid)'
   ],
 
+  /**
+   * Setup editable state observer
+   * Called by Polymer on attach
+   * @return {undefined}
+   */
   attached() {
     this._observeEditable();
   },
 
+  /**
+   * Clean up Simpla observers
+   * Called by Polymer on detach
+   * @return {undefined}
+   */
   detached() {
     Object.keys(this._observers)
       .forEach(observer => {
@@ -26,17 +44,30 @@ export default {
       });
   },
 
+  /**
+   * Init the UID whenever it changes
+   * @param  {String} uid Current value of UID prop
+   * @return {undefined}
+   */
   _initUid(uid) {
+    // Get data for UID
     Simpla.get(uid)
       .then(item => {
         this._value = item.data.markdown
       });
 
+    // Setup data observer for future changes
     this._observeBuffer(uid);
   },
 
+  /**
+   * Set internal value to Simpla on change
+   * @param {String} value Internal markdown source
+   * @param {String} uid   Element UID
+   * @return {Promise}
+   */
   _setData(value, uid) {
-    Simpla.set(uid, {
+    return Simpla.set(uid, {
       type: 'Article',
       data: {
         markdown: value
@@ -44,11 +75,17 @@ export default {
     });
   },
 
+  /**
+   * Data buffer observer
+   * @param  {String} uid UID to observe in buffer
+   * @return {undefined}
+   */
   _observeBuffer(uid) {
     if (!uid) {
       return;
     }
 
+    // Clean up any old observers
     if (this._observers.buffer) {
       this._observers.buffer.unobserve();
     }
@@ -60,9 +97,15 @@ export default {
     });
   },
 
+  /**
+   * Editable state observer
+   * @return {undefined}
+   */
   _observeEditable() {
+    // Get initial value of editable
     this.editable = Simpla.getState('editable');
 
+    // Clean up any old observers
     if (this._observers.editable) {
       this._observers.editable.unobserve();
     }
